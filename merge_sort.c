a308b663e443f25b036267bcaa2a95f89542d205
axenhook <15714832@qq.com>
2020-12-10 09:21:35 +0100

    modify code

--- merge_sort.c
+++ merge_sort.c
@@ -6,189 +6,236 @@
 #include <string.h>
 #include <assert.h>
 #include <time.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-typedef int32_t key_t;
-typedef int32_t value_t;
+typedef uint32_t tuple_key_t;
+typedef uint32_t tuple_value_t;
 
 typedef struct  {
-	key_t   key;
-	value_t value;
+    tuple_key_t   key;
+    tuple_value_t value;
 } tuple_t;
 
-void merge(tuple_t *a, int left, int mid, int right, tuple_t *tmp) {
-	int i = left;
-	int j = mid;
-	int k = left;
+void merge(tuple_t *a, uint32_t left, uint32_t mid, uint32_t right, tuple_t *tmp) {
+    uint32_t i = left;
+    uint32_t j = mid;
+    uint32_t k = left;
 
-	while (i < mid && j < right) {
-		if (a[i].key < a[j].key)
-			tmp[k++] = a[i++];
-		else
-			tmp[k++] = a[j++];
-	}
+    while (i < mid && j < right) {
+        if (a[i].key < a[j].key)
+            tmp[k++] = a[i++];
+        else
+            tmp[k++] = a[j++];
+    }
 
-	while (i < mid)
-		tmp[k++] = a[i++];
+    while (i < mid)
+        tmp[k++] = a[i++];
 
-	while (j < right)
-		tmp[k++] = a[j++];
-
-//	memcpy(a + left, tmp + left, sizeof(tuple_t) * (right - left));
+    while (j < right)
+        tmp[k++] = a[j++];
 }
+
+#if 0 // change algo method
 
 // recursive
-void merge_sort(tuple_t *a, int left, int right, tuple_t *tmp) {
-	int mid;
-	if (left < right - 1) {
-		mid = (right + left) >> 1;
-		merge_sort(a, left, mid, tmp);
-		merge_sort(a, mid, right, tmp);
-		merge(a, left, mid, right, tmp);
-		memcpy(a + left, tmp + left, sizeof(tuple_t) * (right - left));
-	}
+void __merge_sort(tuple_t *a, uint32_t left, uint32_t right, tuple_t *tmp) {
+    uint32_t mid;
+    if (left < right - 1) {
+        mid = (right + left) >> 1;
+        __merge_sort(a, left, mid, tmp);
+        __merge_sort(a, mid, right, tmp);
+        merge(a, left, mid, right, tmp);
+        memcpy(a + left, tmp + left, sizeof(tuple_t) * (right - left));
+    }
 }
+
+static inline void merge_sort(tuple_t *a, uint32_t len, tuple_t *tmp) {
+    __merge_sort(a, 0, len, tmp);
+}
+
+#else
 
 // non-recursive
-void merge_sort2(tuple_t *a, int len, tuple_t *tmp) {
-	if (len <= 1)
-		return;
+void merge_sort2(tuple_t *a, uint32_t len, tuple_t *tmp) {
+    if (len <= 1)
+        return;
 
-	int toggle = 0;
-	tuple_t *src, *dst;
-	for (int width = 1; width < len; width <<= 1) {
-		if (toggle & 1) {
-			src = tmp;
-			dst = a;
-		}
-		else {
-			src = a;
-			dst = tmp;
-		}
-		//clock_t t = clock();
-		for (int i = 0; i < len; i += (width << 1)) {
-			int mid = i + width;
-			if (mid > len)
-				mid = len;
+    uint32_t toggle = 0;
+    tuple_t *src, *dst;
+    for (uint32_t width = 1; width < len; width <<= 1) {
+        if (toggle & 1) {
+            src = tmp;
+            dst = a;
+        }
+        else {
+            src = a;
+            dst = tmp;
+        }
+        //clock_t t = clock();
+        for (uint32_t i = 0; i < len; i += (width << 1)) {
+            uint32_t mid = i + width;
+            if (mid > len)
+                mid = len;
 
-			int right = mid + width;
-			if (right > len)
-				right = len;
+            uint32_t right = mid + width;
+            if (right > len)
+                right = len;
 
-			merge(src, i, mid, right, dst);
-		}
-		//t = clock() - t;
-		//printf("width: %d, time: %f ms\n", width, (float)t * 1000 / CLOCKS_PER_SEC);
-		toggle++;
-	}
+            merge(src, i, mid, right, dst);
+        }
+        //t = clock() - t;
+        //printf("width: %d, time: %f ms\n", width, (float)t * 1000 / CLOCKS_PER_SEC);
+        toggle++;
+    }
 
-	if (toggle & 1)
-		memcpy(a, tmp, len * sizeof(tuple_t));
+    if (toggle & 1)
+        memcpy(a, tmp, len * sizeof(tuple_t));
 }
 
-void init_tuples(tuple_t *a, int size) {
-	for (int i = 0; i < size; i++) {
-		a[i].key = i + 1;
-		//a[i].value = i + 1;
-	}
+#endif
+
+uint32_t merge_join(tuple_t *r, tuple_t *s, uint32_t num_r, uint32_t num_s, void *output) {
+    uint32_t i = 0, j = 0, matches = 0;
+
+    while (i < num_r && j < num_s) {
+        if (r[i].key < s[j].key)
+            i++;
+        else if (r[i].key > s[j].key)
+            j++;
+        else {
+            matches++;
+            j++;
+        }
+    }
+
+    return matches;
 }
 
-void shuffle_tuples(tuple_t *a, int size) {
-	for (int i = 0; i < size; i++) {
-		int j = rand() % size;
-		tuple_t tmp = a[i];
-		a[i] = a[j];
-		a[j] = tmp;
-	}
+#if 1 // change dataset
+
+void init_tuples(tuple_t *a, uint32_t size) {
+    for (uint32_t i = 0; i < size; i++) {
+        a[i].key = i;
+        //a[i].value = i + 1;
+    }
 }
 
-void generate_dataset1(tuple_t *a, int size) {
-	init_tuples(a, size);
-	shuffle_tuples(a, size);
+void shuffle_tuples(tuple_t *a, uint32_t size) {
+    for (uint32_t i = 0; i < size; i++) {
+        uint32_t j = rand() % size;
+        tuple_t tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
 }
 
-void generate_dataset2(tuple_t *a, int size) {
-	for (int i = 0; i < size; i++) {
-		a[i].key = rand() % size;
-		//a[i].value = i + 1;
-	}
+void generate_dataset(tuple_t *a, uint32_t size) {
+    init_tuples(a, size);
+    shuffle_tuples(a, size);
 }
 
-int check_tuples_sorted(tuple_t *a, int size) {
-	for (int i = 0; i < size - 1; i++) {
-		if (a[i].key > a[i + 1].key)
-			return 0; // not sorted
-	}
+bool is_tuples_sorted(tuple_t *a, uint32_t size) {
+    for (uint32_t i = 0; i < size - 1; i++) {
+        if (a[i].key + 1 != a[i + 1].key) {
+            printf("i: %u, key: %u, i+1: %u, key: %u\n", i, a[i].key, i+1, a[i+1].key);
+            return false; // not sorted
+        }
+    }
 
-	return 1; // sorted
+    return true; // sorted
 }
 
-void print_tuples(tuple_t *a, int size) {
-	if (size > 16)
-		size = 16;
-	printf("first %d elements: ", 16);
-	for (int i = 0; i < size; i++) {
-		printf("%d ", a[i].key);
-	}
-	printf("\n");
+#else
+
+void generate_dataset(tuple_t *a, uint32_t size) {
+    for (uint32_t i = 0; i < size; i++) {
+        a[i].key = rand() % size;
+        //a[i].value = i + 1;
+    }
 }
 
-int merge_join(tuple_t *tuplesR, tuple_t *tuplesS, int numR, int numS, void *output) {
-	int i = 0, j = 0, matches = 0;
+bool is_tuples_sorted(tuple_t *a, uint32_t size) {
+    for (uint32_t i = 0; i < size - 1; i++) {
+        if (a[i].key > a[i + 1].key) {
+            printf("i: %u, key: %u, i+1: %u, key: %u\n", i, a[i].key, i+1, a[i+1].key);
+            return false; // not sorted
+        }
+    }
 
-	while (i < numR && j < numS) {
-		if (tuplesR[i].key < tuplesS[j].key)
-			i++;
-		else if (tuplesR[i].key > tuplesS[j].key)
-			j++;
-		else {
-			matches++;
-			j++;
-		}
-	}
-
-	return matches;
+    return true; // sorted
 }
+
+#endif
+
+#if 1
+
+void print_tuples(tuple_t *a, uint32_t size) {
+    if (size > 16)
+        size = 16;
+    printf("first %d elements: ", 16);
+    for (uint32_t i = 0; i < size; i++) {
+        printf("%d ", a[i].key);
+    }
+    printf("\n");
+}
+
+#else
+
+void print_tuples(tuple_t *a, uint32_t size) {
+    printf("%p, total %d", a, size);
+    for (uint32_t i = 0; i < size; i++) {
+        if (i % 16 == 0)
+            printf("\n%08x: ", i);
+        printf("%08x ", a[i].key);
+    }
+    printf("\n");
+}
+
+#endif
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		printf("usage: %s tuples_size", argv[0]);
-		return -1;
-	}
+    if (argc < 2) {
+        printf("usage: %s tuples_size", argv[0]);
+        return -1;
+    }
 
-	int size = atoi(argv[1]);
-	assert(size > 0);
+    int size = atoi(argv[1]);
+    assert(size > 0);
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	tuple_t *a = malloc(size * sizeof(tuple_t));
-	assert(a != NULL);
+    tuple_t *a = malloc(size * sizeof(tuple_t));
+    assert(a != NULL);
 
-	tuple_t *b = malloc(size * sizeof(tuple_t));
-	assert(b != NULL);
+    tuple_t *b = malloc(size * sizeof(tuple_t));
+    assert(b != NULL);
 
-	printf("tuples size: %d, tuples memory: %f MB\n", size, (float)size * sizeof(tuple_t) / 1024 / 1024);
+    printf("tuples size: %d, tuples memory: %f MB\n", size, (float)size * sizeof(tuple_t) / 1024 / 1024);
 
-	generate_dataset1(a, size);
-	generate_dataset1(b, size);
-	print_tuples(a, size);
+    generate_dataset(a, size);
+    generate_dataset(b, size);
+    print_tuples(a, size);
 
-	tuple_t *tmp = malloc(size * sizeof(tuple_t));
-	assert(tmp != NULL);
-	memset(tmp, 0, size * sizeof(tuple_t));
+    tuple_t *tmp = malloc(size * sizeof(tuple_t));
+    assert(tmp != NULL);
+    memset(tmp, 0, size * sizeof(tuple_t));
 
-	printf("begin merge sort and merge join\n");
+    printf("begin merge sort and merge join\n");
 
-	clock_t t = clock();
-	//merge_sort(a, 0, size, tmp);
-	merge_sort2(a, size, tmp);
-	merge_sort2(b, size, tmp);
-	int matches = merge_join(a, b, size, size, tmp);
-	t = clock() - t;
-	printf("time: %f ms, matches: %d\n", (float)t * 1000 / CLOCKS_PER_SEC, matches);
+    clock_t t = clock();
+    merge_sort(a, size, tmp);
+    merge_sort(b, size, tmp);
 
-	print_tuples(a, size);
-	assert(check_tuples_sorted(a, size));
+    uint32_t matches = merge_join(a, b, size, size, tmp);
 
-	return 0;
+    t = clock() - t;
+    printf("time: %f ms, matches: %d\n", (float)t * 1000 / CLOCKS_PER_SEC, matches);
+
+    print_tuples(a, size);
+    assert(is_tuples_sorted(a, size));
+    assert(is_tuples_sorted(b, size));
+
+    return 0;
 
 }
